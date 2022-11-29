7b6d27a812fb5c3a5c13aae7fcec987d9d080bfb
zengzhengxiong <zengzhengxiong050@chinasoftinc.com>
2022-11-29 10:24:54 +0800

    编写测试文件

--- sort/sort.go
+++ sort/sort.go
@@ -1,35 +1,35 @@
 package sort
 
-// BubbleSort 冒牌排序，核心在于相邻两个数字比较交换位置，时间复杂度为O(n^2)
+// BubbleSort 冒牌排序，核心在于相邻两个数字比较交换位置，时间复杂度为O(n^2)，空间复杂度为O(1)
 func BubbleSort(ary []int) []int {
-	for i := len(ary) - 1; i > 0; i-- {
-		for j := 0; j < i; j++ {
+	for i := range ary {
+		for j := 0; j < len(ary)-1-i; j++ {
 			if ary[j] > ary[j+1] {
-				ary[j+1], ary[j] = ary[j], ary[j+1]
+				ary[j], ary[j+1] = ary[j+1], ary[j]
 			}
 		}
 	}
 	return ary
 }
 
-// SelectSort 选择排序，冒泡升级版，旨在把数组中最大（最小）一一选择出来放在前面
+// SelectSort 选择排序，冒泡升级版，旨在把数组中最大（最小）一一选择出来放在前面，时间复杂度为O(n^2)，空间复杂度为O(1)
 func SelectSort(ary []int) []int {
-	var min int
-	for i := 0; i < len(ary)-1; i++ {
-		min = i
-		for j := i + 1; j < len(ary); j++ {
-			if ary[min] > ary[j] {
-				min = j
+	length := len(ary)
+	for i := 0; i < length; i++ {
+		index := i
+		for j := i; j < length; j++ {
+			if ary[j] < ary[index] {
+				index = j
 			}
 		}
-		if min != i {
-			ary[min], ary[i] = ary[i], ary[min]
+		if index != i {
+			ary[index], ary[i] = ary[i], ary[index]
 		}
 	}
 	return ary
 }
 
-// InsertionSort 插入排序，构建有序序列，对于未排序序列，在已排序序列中从后往前扫描，找到相应的位置并插入
+// InsertionSort 插入排序，构建有序序列，对于未排序序列，在已排序序列中从后往前扫描，找到相应的位置并插入，时间复杂度为O(n^2)，空间复杂度为O(1)
 func InsertionSort(ary []int) []int {
 	for i := 1; i < len(ary); i++ {
 		temp := ary[i]
@@ -43,45 +43,99 @@ func InsertionSort(ary []int) []int {
 	return ary
 }
 
-// MergeSort 把序列拆成子序列，先把子序列排序，再合并，通常使用递归的方式
+// MergeSort 把序列拆成子序列，先把子序列排序，再合并，通常使用递归的方式，时间复杂度O(nlogn)，空间复杂度O(n)
 func MergeSort(ary []int) []int {
 	temp := make([]int, len(ary))
-	internalMergeSort(ary, temp, 0, len(ary)-1)
+	internalMerge(ary, temp, 0, len(ary)-1)
 	return ary
 }
 
-func internalMergeSort(ary, temp []int, left, right int) {
+func internalMerge(ary, temp []int, left, right int) {
 	if left < right {
 		middle := (left + right) / 2
-		internalMergeSort(ary, temp, left, middle)
-		internalMergeSort(ary, temp, middle+1, right)
-		mergeSortedArray(ary, temp, left, middle, right)
+		internalMerge(ary, temp, left, middle)
+		internalMerge(ary, temp, middle+1, right)
+		mergeAry(ary, temp, left, middle, right)
 	}
 }
-func mergeSortedArray(ary, temp []int, left, middle, right int) {
-	i, j, k := left, middle+1, 0
 
+func mergeAry(ary, temp []int, left, middle, right int) {
+	i, j, k := left, right, 0
 	for i <= middle && j <= right {
-		if ary[i] < ary[j] {
+		if ary[i] <= ary[j] {
 			temp[k] = ary[i]
+			k++
 			i++
 		} else {
-			temp[k] = ary[j]
+			ary[k] = ary[j]
+			k++
 			j++
 		}
-		k++
 	}
 	for i <= middle {
 		temp[k] = ary[i]
-		k++
 		i++
+		k++
 	}
 	for j <= right {
 		temp[k] = ary[j]
-		k++
 		j++
+		k++
 	}
-	for index := 0; index < k; index++ {
-		ary[left+index] = temp[index]
+	for i2 := 0; i2 < k; i2++ {
+		ary[left+i2] = temp[i2]
 	}
+}
+
+// QuickSort 快速排序，选定一个基准值，然后根据这个基准值分为两部分，然后一次对这两个部分进行递归处理，时间复杂度O(nlogn)，空间复杂度O(nlogn)
+func QuickSort(ary []int) []int {
+	qSort(ary, 0, len(ary)-1)
+	return ary
+}
+
+func qSort(ary []int, low, high int) {
+	if low > high {
+		return
+	}
+	pivot := partition(ary, low, high)
+	qSort(ary, low, pivot-1)
+	qSort(ary, pivot+1, high)
+}
+
+func partition(ary []int, low, high int) int {
+	pivot := ary[low]
+	for low < high {
+		for low < high && ary[high] >= pivot {
+			high--
+		}
+		ary[low] = ary[high]
+		for low < high && ary[low] <= pivot {
+			low++
+		}
+		ary[high] = ary[low]
+	}
+	ary[low] = pivot
+	return low
+}
+
+// ShellSort 希尔排序
+func ShellSort(arr []int) []int {
+	length := len(arr)
+	gap := 1
+	for gap < length/3 {
+		gap = gap*3 + 1
+	}
+	for gap > 0 {
+		for i := gap; i < length; i++ {
+			temp := arr[i]
+			j := i - gap
+			for j >= 0 && arr[j] > temp {
+				arr[j+gap] = arr[j]
+				j -= gap
+			}
+			arr[j+gap] = temp
+		}
+		gap = gap / 3
+	}
+	return arr
 }
---
+++ sort/sort_test.go
@@ -0,0 +1,22 @@
+package sort
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	fmt.Println(BubbleSort([]int{-23, -34, -5, -23, 23, 56, 1234, 63, 56, 734, 36, 24, 2334, 2, 24, 624, 12343, 2482, -234, -234, 234}))
+}
+
+func TestSelectSort(t *testing.T) {
+	fmt.Println(SelectSort([]int{-23, -34, -5, -23, 23, 56, 1234, 63, 56, 734, 36, 24, 2334, 2, 24, 624, 12343, 2482, -234, -234, 234}))
+}
+
+func TestInsertionSort(t *testing.T) {
+	fmt.Println(InsertionSort([]int{-23, -34, -5, -23, 23, 56, 1234, 63, 56, 734, 36, 24, 2334, 2, 24, 624, 12343, 2482, -234, -234, 234}))
+}
+
+func TestMergeSort(t *testing.T) {
+	fmt.Println(MergeSort([]int{-23, -34, -5, -23, 23, 56, 1234, 63, 56, 734, 36, 24, 2334, 2, 24, 624, 12343, 2482, -234, -234, 234}))
+}
